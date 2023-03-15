031a9311c882aa39b2a87da79ab060e962bf5546
Ramesh Vyas <rameshovyas@gmail.com>
2023-03-15 15:09:44 +0530

    Added Item Entity

---
+++ Entities/Item.cs
@@ -0,0 +1,15 @@
+namespace MyProject.Catalog.Entities
+{
+    public class Item
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public DateTimeOffset CreatedDate { get; set; }
+    }
+}
