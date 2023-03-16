31cf8389965879a115928c4a0ae3f81f685188bf
Ramesh Vyas <rameshovyas@gmail.com>
2023-03-16 08:52:15 +0530

    Added methods for CRUD operations in ItemsRepository

--- Repositories/ItemsRepository.cs
+++ Repositories/ItemsRepository.cs
@@ -14,7 +14,47 @@ namespace MyProject.Catalog.Service.Repositories
             var mongoClient = new MongoClient("mongodb://localhost:27017"); // mongo db connection
             var database = mongoClient.GetDatabase("Catalog"); // database name
             dbCollection = database.GetCollection<Item>(collectionName); // database collection (tables)
-            
+
+        }
+
+
+        //Method to get all items from collection
+
+        public async Task<IReadOnlyCollection<Item>> GetAllAsync()
+        {
+            return await dbCollection.Find(filterBuilder.Empty).ToListAsync();
+        }
+
+        public async Task<Item> GetAsync(Guid id)
+        {
+            FilterDefinition<Item> filter = filterBuilder.Eq(entity => entity.Id, id);
+            return await dbCollection.Find(filter).FirstOrDefaultAsync();
+        }
+
+        public async Task CreateAsync(Item entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException(nameof(entity));
+            }
+
+            await dbCollection.InsertOneAsync(entity);
+        }
+
+        public async Task UpdateAsync(Item entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException(nameof(entity));
+            }
+            FilterDefinition<Item> filter = filterBuilder.Eq(exitingEntity => exitingEntity.Id, entity.Id);
+            await dbCollection.ReplaceOneAsync(filter, entity);
+        }
+
+        public async Task RemoveAsync(Guid id)
+        {
+            FilterDefinition<Item> filter = filterBuilder.Eq(entity => entity.Id, id);
+            await dbCollection.DeleteOneAsync(filter);
         }
     }
 }
